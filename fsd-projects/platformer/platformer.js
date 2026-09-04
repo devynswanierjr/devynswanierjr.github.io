11dff4504f3bbd3d5d7d6820dd05de1deda589da
devynswanierjr <30dswanier@gmail.com>
2026-09-04 13:04:13 +0000

    another messgae

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -49,12 +49,13 @@ $(function () {
     // TODO 3 - Create Collectables
     createCollectable("steve",1150,50);
     createCollectable("diamond",200 , 170, .5, .7);
-     createCollectable("diamond", 250, 180, .5 , .09)
+     createCollectable("kennedi", 250, 180, .5 , .09)
     
     // TODO 4 - Create Cannons
      createCannon("top", 150, 200);
 createCannon("right", 300, 2000);
 createcannon("left", 150, 400)
+createcannon("bottom, 700, 500");
 
     
     
